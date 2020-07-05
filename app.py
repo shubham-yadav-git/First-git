aadb6723952ee2a2f5bbb8f9b22cd363f8e66dea
shubham-yadav-git <51941625+shubham-yadav-git@users.noreply.github.com>
2020-07-05 12:46:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("Hello")
+    return render_template("index.html")
     
-if __name__ == "__main__":
-    app.run(debug=True)
+app.run(debug=True)
