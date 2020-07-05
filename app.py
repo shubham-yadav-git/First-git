a0c5d0982392781d492984ac6f7a5a4063888654
shubham-yadav-git <51941625+shubham-yadav-git@users.noreply.github.com>
2020-07-05 15:40:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, request, jsonify, render_template
 app = Flask(__name__)
 
 
-@app.route('/',methods=["GET"])
+@app.route('//',methods=["GET"])
 def home():
     return ("<HTML><BODY>Hello</BODY></HTML>")
     
